eae98877c14d000560274ccdc420e5ab977a66f6
karfisk <karfi.sk@gmail.com>
2025-09-21 22:44:40 +0200

    Feat: started working on parser. Planing how to do things, subject to change

---
+++ src/parser.c
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////
+// filename: common.c                  	    //
+// IFJ_prekladac	varianta - vv-BVS   	//
+// Authors:						  			//
+//  * Jaroslav Mervart (xmervaj00) / 5r6t 	//
+//  * Veronika Kubová (xkubovv00) / Veradko //
+//  * Jozef Matus (xmatusj00) / karfisk 	//
+//  * Jan Hájek (xhajekj00) / Wekk 			//
+//////////////////////////////////////////////
+
+#include "../include/common.h"
+#include "../include/lex.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void parser(FILE *file)
+{
+    TokenPtr nextToken = lexer(file); // lookahead -> maybe i shouldn`t declare nextToken here, something to think about
+    if (PROGRAM(nextToken, file))
+    {
+        return 0;
+    }
+    else
+    {
+        return 1; // change to error code that is most fitting
+    }
+}
+
+int PROGRAM(TokenPtr nextToken, FILE *file)
+{
+    if (PROLOG(nextToken, file))
+    {
+        if (CLASS(nextToken, file))
+        {
+            return 0;
+        }
+        else
+        {
+            return 1; // change to error code that is most fitting -> code will be 2. Should think about how to handle error better -> maybe use function here?( I wouldn`t need to have returned type of int)
+        }
+    }
+    else
+    {
+        return 1; // change to error code that is most fitting
+    }
+}
+
+int PROLOG(TokenPtr nextToken, FILE *file)
+{
+    // TODO change this to be more effective using function while_function
+
+    /*
+    match("import");
+    match("\"ifj_25\"");
+    match("for");
+    match("Ifj");
+    */
+
+    // here should be something to detected End Of Line (EOL)
+    return 0;
+}
+
+int CLASS(TokenPtr nextToken, FILE *file)
+{
+    // TODO make while and array filled with "class Program { EOL". While would iterated until array is passted then function FUNCTIONS is called and after that check for }
+    match("class", nextToken->data);
+    nextToken = lexer(file); // lookahead
+    match("Program", nextToken->data);
+    nextToken = lexer(file);
+    match("{", nextToken->data);
+    nextToken = lexer(file);
+    // here should be something to detected End Of Line (EOL)
+    nextToken = lexer(file);
+    FUNCTIONS(nextToken);
+    nextToken = lexer(file);
+    match("}", nextToken->data);
+    return 0;
+}
+
+int FUNCTIONS(TokenPtr nextToken)
+{
+    if (match("static", nextToken->data))
+    {
+    }
+}
+
+// using strcmp compares target string with token. If token isn`t same as target string error is send to stderr output
+int match(char target[], char token_data[])
+{
+}
+
+// helping function for more pleasing way to check matches and for updating nextToken
+int while_function(char targetStrArr[], FILE *file, TokenPtr nextToken)
+{
+}
